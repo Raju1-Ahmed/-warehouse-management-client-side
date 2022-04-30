80b13cfc6d27457ac876fd0b6f4b3c8a485b44aa
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-05-01 01:22:16 +0600

    add a Carousel

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Pages/Header/Header';
 import Footer from './Pages/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <div>
-           <Header></Header>
-    <h2 className=''>Check</h2>
-    <Footer></Footer>
+        <Header></Header>
+      <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+      </Routes>
+        <Footer></Footer>
       </div>
     </div>
   );
