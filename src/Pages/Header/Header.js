89516bdcfc40254c3d96b651eb35a8003330a0e0
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-05-07 17:57:48 +0600

    nav finaly desig complate

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -4,6 +4,8 @@ import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
+import './Header.css'
+import navlogo from '../../image/menu-logo/menuIcon.png'
 
 const Header = () => {
   const [user] = useAuthState(auth);
@@ -11,21 +13,13 @@ const Header = () => {
     signOut(auth);
   }
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar className="bg" collapseOnSelect expand="lg" sticky='top'  bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/"><img height={50} src={navlogo} alt="" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
           </Nav>
           <Nav>
             {
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
+import './Banner.css'
 
 import banner1 from '../../../image/Banner/images.jpg'
 import banner2 from '../../../image/Banner/images1.jpg'
@@ -20,9 +21,9 @@ const Banner = () => {
                 alt="First slide"
             />
             <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
+            <h3 className='clr'>MEN’S SHOPPING AT MYNTRA: A SUPERIOR EXPERIENCE</h3>
+                <p  className='clr'>Myntra is one of the best sites when it comes to online shopping for men..s</p>
+           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
@@ -32,9 +33,9 @@ const Banner = () => {
             />
 
             <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
+            <h3 className='clr'>ONLINE SHOPPING FOR MEN: OPTIONS UNLIMITED</h3>
+                <p className='clr'>At Myntra, our online shopping fashion for men collection features plenty of options to create multiple outfits. At our men’s online shop we have brought together an exhaustive range of products from the best men’s brands. Here is a list of must-haves from the wide variety of awesome products at Myntra:.</p>
+           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
@@ -44,9 +45,9 @@ const Banner = () => {
             />
 
             <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            <h3 className='clr'>MEN’S SHOPPING MADE EASY AT MYNTRA</h3>
+                <p className='clr'>
+                Myntra is the most convenient men’s online store, what with our simplified shopping and payment procedures. With just a few clicks of the mouse or taps on your smartphone, you can buy your favorites from the best men’s brands right away.
                 </p>
             </Carousel.Caption>
         </Carousel.Item>
