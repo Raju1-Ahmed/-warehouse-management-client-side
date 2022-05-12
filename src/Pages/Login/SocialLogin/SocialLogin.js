117730b538c2b226b343ce74813c3d23a25e04fc
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-05-12 12:05:47 +0600

    add style

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -27,27 +27,27 @@ const SocialLogin = () => {
     return (
         <div>
             <div className='d-flex align-items-center'>
-                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-danger w-50'></div>
                 <p className='mt-2 px-2'>or</p>
-                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-danger w-50'></div>
             </div>
             {errorElement}
             <div>
                 <button
                 onClick={() => signInWithGoogle()}
-                    className='btn btn-warning   col-md-12 my-2'>
+                    className='btn btn-danger w-50 d-block mx-auto my-2'>
                     <img style={{ width: '30px' }} src={google} alt="" />
                     <span className='px-2'>Google Sign In</span>
                 </button>
-                {/* <button className='btn btn-warning  col-md-12 my-2'>
-                    <img style={{ width: '20px' }} src={facebook} alt="" />
-                    <span className='px-2'>Facebook-SignIn</span>
+                <button className='btn btn-danger w-50 d-block mx-auto my-2'>
+                    <img style={{ width: '30px' }} src={facebook} alt="" />
+                    <span className='px-2'>Facebook Sign In</span>
                 </button>
                 <button
-                    className='btn btn-warning  col-md-12 my-2'>
+                    className='btn btn-danger w-50 d-block mx-auto'>
                     <img style={{ width: '30px' }} src={github} alt="" />
                     <span className='px-2'>Github Sign In</span>
-                </button> */}
+                </button>
             </div>
         </div>
     );
